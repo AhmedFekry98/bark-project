b098260479348dfab1e43e5bbbb89439b24bdf17
abdulrhman-developer0 <tknwtknwlwjy10@gmail.com>
2024-08-22 19:06:11 +0300

    fensh profile updates

--- Modules/Auth/Http/Requests/UpProfileRequest.php
+++ Modules/Auth/Http/Requests/UpProfileRequest.php
@@ -14,42 +14,23 @@ class UpProfileRequest extends FormRequest
     public function rules()
     {
         $role = $this->user()->role;
-        $condationalRules = [];
 
-        if ($role ==  'customer') {
-            // $condationalRules['extra.address']           = ['nullable'];
-        }
+        return array_merge(
+            [
+                'name'              => ['nullable', 'string', 'min:3', 'max:25'],
+                'phone'             => ['nullable', 'string', 'min:9', 'max:13'],
+                'email'             => ['nullable', 'email', 'min:5', 'max:100', 'unique:users,email'],
+                'password'          => ['nullable', 'string', 'min:6', 'max:16'],
+            ],
 
-        if ($role == 'restaurant') {
-            $condationalRules['openTime']           = ['nullable', 'string', 'date_format:h:i A'];
-            $condationalRules['closeTime']          = ['nullable', 'string', 'date_format:h:i A'];
-            $condationalRules['restaurantImage']    = ['nullable', 'image', 'max:4096'];
-            $condationalRules['customDomain']       = ['nullable', 'url'];
+            $role == 'provider' ? [
+                "categoryId"       =>  ['nullable', 'integer', 'exists:categories,id'],
+                'companyName'       => ['nullable', 'string'],
+                'companyWebsite'    => ['nullable', 'url'],
+                'companySize'       => ['nullable', 'string'],
+            ]: [],
 
-            # socail media links.
-            $condationalRules['socialmediaLinks.facebook']    = ['nullable', 'url', 'max:350'];
-            $condationalRules['socialmediaLinks.twitter']     = ['nullable', 'url', 'max:350'];
-            $condationalRules['socialmediaLinks.instagram']    = ['nullable', 'url', 'max:350'];
-            $condationalRules['socialmediaLinks.youtube']    = ['nullable', 'url', 'max:350'];
-            $condationalRules['socialmediaLinks.tiktok']      = ['nullable', 'url', 'max:350'];
-        }
-
-
-        return [
-            'firstName'         => ['nullable', 'string', 'min:3', 'max:25'],
-            'lastName'          => ['nullable', 'string', 'min:3', 'max:25'],
-            'phoneNumber'      => ['nullable', 'string', 'min:9', 'max:13'],
-            'email'              => ['nullable', 'email', 'min:5', 'max:100', 'unique:users,email'],
-            'password'          => ['nullable', 'string', 'min:6', 'max:16'],
-            // 'asRestaurant'      => ['nullable', 'boolean'],
-            'restaurantName'    => ['exclude_if:asRestaurant,false', 'nullable', 'string', 'min:1', 'max:50'],
-            'image'             => ['nullable', 'image'],
-            'address'     => ['nullable'],
-            'location'    => ['nullable', 'url'],
-
-            # condational rules.
-            ...$condationalRules
-        ];
+        );
     }
 
     /**
